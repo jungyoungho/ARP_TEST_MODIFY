9ae751d496c8fe85cb5ff6f2445c5700a88930dd
jungyoungho <sladkwnlwnl@naver.com>
2017-04-05 02:02:34 +0900

    Update mac.cpp
    delete print

--- ARP/modify/mac.cpp
+++ ARP/modify/mac.cpp
@@ -5,8 +5,7 @@
 
 Mac& Mac::operator =(const char *str){
     sscanf((const char*)str, "%x:%x:%x:%x:%x:%x",&val[0],&val[1],&val[2],&val[3],&val[4],&val[5]);
-    for(int i=0; i<6; i++)
-        printf("%x",val[i]);
+
     return *this;
 }
 
